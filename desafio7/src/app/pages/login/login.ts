0ccd1442a9e1b19117e0500171ecdb1c2c9f1c7a
moniquemedeiros-design <monique.medeiros@ba.estudante.senai.br>
2026-06-16 16:18:22 -0300

    commit7

--- desafio7/src/app/pages/login/login.ts
+++ desafio7/src/app/pages/login/login.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+
+  email = '';
+  senha = '';
+  logarAutomaticamente = false;
+
+}
